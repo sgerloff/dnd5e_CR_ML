a05de5a92a337cae134093544b99f40b4d0234f7
Sascha Gerloff <sascha.gerloff@t-online.de>
2020-12-05 12:57:47 +0100

    Implemented bare bones logistic regression for three key numeric features for sanity check -> high bias; needs more features

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import glob
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 list_of_bestiaries = glob.glob("data/bestiary/*.json")
 
@@ -19,49 +20,17 @@ for bestiary in list_of_bestiaries:
             elif "_copy" in mon:
                 print("Omit copied monster %s" % mon["name"])
             elif "cr" in mon:
-                monsters.append(mon)
+                if mon["cr"] == "Unknown":
+                    print("Omit monster %s with unknown cr" % mon["name"])
+                elif type(mon["cr"]) == dict:
+                    tmp = mon["cr"]["cr"]
+                    print(tmp)
+                    mon["cr"] = tmp
+                    monsters.append(mon)
+                else:
+                    monsters.append(mon)
 
-print("I have read %d stat blocks of monsters!" % (len(monsters)) )
-
-
-class CleanMonsterData:
-    data = {}
-    def __init__(self, monster):
-        self.data = monster.copy()
-
-    def drop_insignificant_keys(self):
-        for key in ["isNpc", "source", "page", "passive", "languages", "hasToken", "languageTags", "miscTags", "senses", "_isCopy", "familiar"]:
-            self.data.pop(key, None)
-
-    def reduce_attributes(self):
-        sum_of_attributes = 0
-        for attribute in ["str", "dex", "con", "wis", "int", "cha"]:
-            if attribute in self.data:
-                sum_of_attributes += self.data[attribute]
-                self.data.pop(attribute)
-            else:
-                print("Missing attribute %s!" % attribute)
-        self.data["attribute_sum"] = sum
-
-    def reduce_skill(self):
-        if "skill" in self.data:
-            skill_proficiencies = len(self.data["skill"])
-            self.data.pop("skill")
-            self.data["skill_proficiencies"] = skill_proficiencies
-        else:
-            self.data["save_proficiencies"] = 0
-
-    def reduce_save(self):
-        if "save" in self.data:
-            save_proficiencies = len(self.data["save"])
-            self.data.pop("save")
-            self.data["save_proficiencies"] = save_proficiencies
-        else:
-            self.data["save_proficiencies"] = 0
-
-    def reduce_senseTags(self):
-        if not "senseTags" in self.data:
-            self.data["senseTags"] = []
+print("I have read %d stat blocks of monsters!" % (len(monsters)))
 
 
 def getUniqueEntries(list, string):
@@ -69,23 +38,30 @@ def getUniqueEntries(list, string):
     for element in list:
         if string in element:
             for sub_element in element[string]:
-                if not sub_element in used:
+                if sub_element not in used:
                     used.append(sub_element)
     return used
 
-# used = []
-# for mon in monsters:
-#     if "actionTags" in mon:
-#         for x in mon["actionTags"]:
-#             if not x in used:
-#                 used.append(x)
 
-print( getUniqueEntries(monsters, "senseTags") )
+#Plot Histogram:
+# from utility.plot_monster_cr_histogram import plot_monster_cr_histogram
+# plot_monster_cr_histogram(monsters, "data/cr_histogram.png")
 
-# data = CleanMonsterData(monsters[0])
-# data.reduce_senses()
-# print(data.data["senseTag1"])
+#Extract features and write into X,y
+from utility.MonsterFeatureExtrator import MonsterFeatureExtractor
+import numpy as np
 
-# for i in monsters:
-#     if not "senseTags" in i:
-#         print(i["name"])
+input_data = list()
+input_target = list()
+for mon in monsters:
+    clean = MonsterFeatureExtractor(mon)
+    input_data.append(clean.get_feature_list())
+    input_target.append(mon["cr"])
+
+X = np.array(input_data)
+y = input_target
+
+from models.logistic_regression import logistic_regression_model
+model = logistic_regression_model(X,y)
+model.plot_learning_curve( 'data/leaning_curve_logistic_regression.png' )
+
---
+++ models/logistic_regression.py
@@ -0,0 +1,52 @@
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+class logistic_regression_model:
+    X_train = np.array(1)
+    X_test = np.array(1)
+    y_train = list()
+    y_test = list()
+
+    number_of_samples = list()
+    train_scores = list()
+    test_scores = list()
+
+    def __init__(self, X, y):
+        #Rescale Features:
+        scaler = preprocessing.StandardScaler().fit(X)
+        X_scale = scaler.transform(X)
+        #Split data set:
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X_scale, y, test_size=0.33, random_state=0)
+
+    def plot_learning_curve(self, output_str):
+        self.train_learning_curve()
+        plt.figure()
+        plt.plot(self.number_of_samples, self.train_scores, label="Training")
+        plt.plot(self.number_of_samples, self.test_scores, label="Testing")
+        plt.xlim(0, 900)
+        plt.ylim(0, 1)
+        plt.xlabel("# of samples")
+        plt.ylabel("Accuracy Score")
+        plt.title("Learning Curve")
+        plt.legend()
+        plt.savefig(output_str)
+        plt.show()
+
+    def train_learning_curve(self):
+        self.number_of_samples = list()
+        self.train_scores = list()
+        self.test_scores = list()
+        for i in range(10, len(self.y_train)):
+            index = i + 1
+            if index % 10 == 0:
+                X_tmp = self.X_train[:index]
+                y_tmp = self.y_train[:index]
+                clf = LogisticRegression(random_state=0).fit(X_tmp, y_tmp)
+                self.number_of_samples.append(index)
+                self.train_scores.append(clf.score(X_tmp, y_tmp))
+                self.test_scores.append(clf.score(self.X_test, self.y_test))
